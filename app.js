e965e2afe6b0451cb6269eae94c12b9836736f3a
Mitch <mboyd@outlook.com.au>
2020-10-03 17:36:52 +1000

    Merged old and new app.js

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
-const Express = require("express");
+const express = require("express");
 const mongo = require("./services/MongoService");
-let app = express();
+let app = new express();
 
 // Setup DB
 mongo.openConnection();
 
 //printing logs
-var log = function(logMessage) {
-        var time = moment().format()
-        console.log('[' + time + ']' + '' + logMessage)
-    }
-    //direct app to fetch relevant files
-app.use(express.static(__dirname + '/Public'));
+var log = function (logMessage) {
+  var time = moment().format();
+  console.log("[" + time + "]" + "" + logMessage);
+};
+//direct app to fetch relevant files
+app.use(express.static(__dirname + "/Public"));
 //set endpoint to html file
-app.get('/Game', function(req, res) {
-    res.sendFile(__dirname + '/Public/HomePage.html');
-
+app.get("/Game", function (req, res) {
+  res.sendFile(__dirname + "/Public/HomePage.html");
 });
-//port
-let port = 3000;
-app.listen(port)
-log(' Server listening on http://' + "localhost" + ':' + port + "/game")
+
+//Server Run
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log("Server listening on http://localhost:" + PORT + "/game");
+});
