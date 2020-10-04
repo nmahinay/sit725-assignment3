e475b3981986ddf7c66610d1851b1e73e2eb057c
amanda.haydaw <amanda.haydaw@accenture.com>
2020-10-04 15:18:28 +1100

    PlayerList UI #4

--- app.js
+++ app.js
@@ -6,19 +6,19 @@ let app = new express();
 mongo.openConnection();
 
 //printing logs
-var log = function (logMessage) {
-  var time = moment().format();
-  console.log("[" + time + "]" + "" + logMessage);
+var log = function(logMessage) {
+    var time = moment().format();
+    console.log("[" + time + "]" + "" + logMessage);
 };
 //direct app to fetch relevant files
 app.use(express.static(__dirname + "/public"));
 //set endpoint to html file
-app.get("/Game", function (req, res) {
-  res.sendFile(__dirname + "/public/HomePage.html");
+app.get("/Game", function(req, res) {
+    res.sendFile(__dirname + "/public/HomePage.html");
 });
 
 //Server Run
 const PORT = 3000;
 app.listen(PORT, () => {
-  console.log("Server listening on http://localhost:" + PORT + "/game");
+    console.log("Server listening on http://localhost:" + PORT + "/game");
 });
